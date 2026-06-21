fc339a264067501ab64270ab4ddaeb5170132a3a
lorena09-15 <enriquezpedrolorenareyna@gmail.com>
2026-06-21 00:32:34 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function mensajeInicio() {
+    alert("Llevate algo bonito por unas tapitas ");
+}
+
+function mensajeSillones() {
+    alert("Algo comodo para ti por una esperanza al planeta");
+}
+
+function mensajeTapetes() {
+    alert("Mas durabilidad y mas verde nuestro entorno");
+}
+
+function mensajeFlores() {
+    alert("Algo bonito para ti, una aportunidad mas al planeta");
+}
